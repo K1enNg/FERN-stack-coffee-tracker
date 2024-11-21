b188472fcf45607932b794e86a202a60d3ee7a91
Kinkin <hiimkien2702@gmail.com>
2024-11-21 02:49:20 -0500

    modified Layout.jsx and Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Layout from './components/Layout'
 import Hero from './components/Hero'
+import CoffeeForm from './components/CoffeeForm'
+import Stats from './components/Stats'
+import History from './components/History'
 
 const App = () => {
 
   const isAuthenticated = false
+
+  const authenticatedContent = (
+    <>
+      <Stats/>
+      <History/>
+    </>
+  )
   return (
     <>
       <Layout>
         <Hero/>
+        <CoffeeForm/>
+        {isAuthenticated && (authenticatedContent)}
       </Layout>
     </>
   )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,11 +4,24 @@ const Layout = (props) => {
     const { children } = props
 
     const header = (
-      <header></header>
+      <header>
+        <div>
+          <h1 className='text-gradient'>Kaffien</h1>
+          <p>For Coffee Insasiate</p>
+        </div>
+        <button>
+          <p>Sign up free</p>
+          <i className="fa-solid fa-mug-hot"></i>
+        </button>
+      </header>
     )
 
     const footer = (
-      <footer></footer>
+      <footer>
+        <p><span className='text-gradient'>Kaffien</span> was made by <a 
+        href='https://www.linkedin.com/in/kien-nguyen-46bab22a1/'>Kien</a> using the <a
+        href='http://www.fantacss.smoljames.com' target='_blank'>FantaCSS</a> design library</p>
+      </footer>
     )
   return (
     <>
